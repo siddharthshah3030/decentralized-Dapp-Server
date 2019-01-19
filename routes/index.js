538a2d3e6925c50e468bec414b44b9e2587000f4
siddharth shah <siddharthshah3030@gmail.com>
2019-01-20 02:24:35 +0530

    initialise and starting

---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+module.exports = router;
+
+
+
+var Web3 = require('web3');
+var web3 = new Web3('http://localhost:8545');
+// or
+var web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+console.log(Web3)
+// change provider
+web3.setProvider('ws://localhost:8546');
+// or
+web3.setProvider(new Web3.providers.WebsocketProvider('ws://localhost:8546'));
+
+// Using the IPC provider in node.js
+var net = require('net');
+var web3 = new Web3('/Users/myuser/Library/Ethereum/geth.ipc', net); // mac os path
+// or
+var web3 = new Web3(new Web3.providers.IpcProvider('/Users/myuser/Library/Ethereum/geth.ipc', net)); // mac os path
+// on windows the path is: "\\\\.\\pipe\\geth.ipc"
+// on linux the path is: "/users/myuser/.ethereum/geth.ipc"
