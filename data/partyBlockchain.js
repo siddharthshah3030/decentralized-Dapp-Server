63cb4f7ccea5ee53d48dff404a7304fa34227784
Shritesh Jamulkar <Shritesh.sj@gmail.com>
2019-01-20 13:56:03 +0530

    khatam

--- data/partyBlockchain.js
+++ data/partyBlockchain.js
@@ -7,16 +7,20 @@ party.find({}, function(err, currentParty) {
     currentParty.forEach(function(partyInstance) {
         Elections.deployed().then(function(instance) {
             var electionInstance = instance;
-            return web3.eth.getAccounts()
+            web3.eth.getAccounts
                 .then(function (accounts) {
-                    console.log(accounts)
+                    console.log(accounts);
                     return electionInstance
-                        .addParty(partyInstance.identity,{from:accounts[0]})
+                        .addParty(parseInt(partyInstance['identity']),{
+                            from: accounts[0]
+                        }).then(function (res) {
+                            console.log(res)
+                        }).catch(function (err) {
+                            console.log(err);
+                        });
             });
-        })
-        
+        });
+        console.log(partyInstance)
 
     });
 });
-
-
--- etherum/web3.js
+++ etherum/web3.js
@@ -1,7 +1,12 @@
 var Web3 = require('web3');
-var ethers = require('ethers');
-const provider = new Web3.providers.HttpProvider(
-    'https://rinkeby.infura.io/v3/6e628e8f3b044d92a2759634a487e04e'
-);
-web3 = new Web3(provider);
+let web3;
+if(typeof window !== 'undefined' && typeof  window.web3 !== 'undefined'){
+    web3 = new Web3(window.web3.currentProvider);
+}
+else {
+    const provider = new Web3.providers.HttpProvider(
+        'https://rinkeby.infura.io/v3/6e628e8f3b044d92a2759634a487e04e'
+    );
+    web3 = new Web3(provider);
+}
 module.exports = web3;
